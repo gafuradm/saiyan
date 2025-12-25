d79c6023036ad9ad3846312582c8008be40c6ec2
Gafur <gafur.adm09adm@yandex.ru>
2025-12-26 02:22:12 +0800

    Initial commit

--- src/main.py
+++ src/main.py
@@ -1,3 +1,6 @@
+import sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).parent.parent))
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
