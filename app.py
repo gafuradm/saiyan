f7ef42b417c84262ac6d67373457bd979051a376
Gafur <gafur.adm09adm@yandex.ru>
2025-12-26 02:48:54 +0800

    Initial commit

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+# Добавляем корень проекта в PATH (это ключевой фикс для Render)
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+
 from src.main import app
 
 if __name__ == "__main__":
